550fa4980bf33944a9147110676f01d8957b90cf
GoopDev <goopdeveloper@gmail.com>
2016-04-18 17:46:10 -0400

    'files'

--- The Owner/core/src/com/jett/game/GameScreen.java
+++ The Owner/core/src/com/jett/game/GameScreen.java
@@ -1,11 +1,26 @@
 package com.jett.game;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.Array;
 
 public class GameScreen implements Screen{
 	
+	private OrthographicCamera camera = new OrthographicCamera(16*10,9*10);
+	private TextureAtlas exampleSpriteSheet = new TextureAtlas(Gdx.files.internal("spriteSheet.txt"));
+	private Array<Sprite> playerSpriteSheet = exampleSpriteSheet.createSprites("exampleSpriteSheet");
+	private SpriteBatch batcher = new SpriteBatch();
+	public float frameIndex;
+	
 	public GameScreen(){
-		
+		for(int i = 0; i < playerSpriteSheet.size; i++){
+			playerSpriteSheet.get(i).setSize(1,1);
+		}
+		batcher.setProjectionMatrix(camera.combined);
 	}
 	
 	@Override
@@ -16,8 +31,10 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
+		frameIndex += delta;
+		batcher.begin();
+		batcher.draw(playerSpriteSheet.get((int)frameIndex).getTexture(), 100, 100);
+		batcher.end();
 	}
 
 	@Override
