357e34211cc49c3dab09cbc0ed4bcc6e3fbbed87
GoopDev <goopdeveloper@gmail.com>
2016-04-18 16:18:08 -0400

    'Anim.java'

--- The Owner/core/src/com/jett/game/Anim.java
+++ The Owner/core/src/com/jett/game/Anim.java
@@ -1,18 +1,20 @@
 package com.jett.game;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Anim {
 	
-	public TextureRegion referenceRegion;
+	public Texture referenceRegion;
 	public TextureRegion[] resultRegion;
 	public int ROWS;
 	public int COLUMNS;
 	
-	public Anim(TextureRegion reference, int ROWS, int COLUMNS){
+	public Anim(Texture reference, int ROWS, int COLUMNS){
 		ROWS = this.ROWS;
 		COLUMNS = this.COLUMNS;
 		referenceRegion = reference;
+		resultRegion = referenceRegion.split(referenceRegion, referenceRegion.getWidth()/ROWS, referenceRegion.getHeight()/COLUMNS);
 	}
 	
 }
