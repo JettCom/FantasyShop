dead7c1a5fb7e61dba0cfc919acb1315d6e4cef9
GoopDev <goopdeveloper@gmail.com>
2016-04-18 22:34:18 -0400

    Modified to properly fit animation

--- The Owner/core/src/com/jett/game/GameScreen.java
+++ The Owner/core/src/com/jett/game/GameScreen.java
@@ -2,23 +2,30 @@ package com.jett.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameScreen implements Screen{
 	
-	public Sprite[] testAnim;
+	public Texture[] testAnim;
 	public SpriteBatch batcher;
+	
+	public OrthographicCamera cam;
 	
 	public float frameIndex;
 	
 	public GameScreen(){
 		batcher = new SpriteBatch();
-		testAnim = new Sprite[4];
-		for(int i = 0; i < 4; i++){
-			testAnim[i].setTexture(new Texture(Gdx.files.internal("SpriteSheet"+(i+1)+".png")));
-		}
+		testAnim = new Texture[5];
+		testAnim[0] = (new Texture(Gdx.files.internal("SpriteSheet1.png")));
+		testAnim[1]  = (new Texture(Gdx.files.internal("SpriteSheet2.png")));
+		testAnim[2] = (new Texture(Gdx.files.internal("SpriteSheet3.png")));
+		testAnim[3] = (new Texture(Gdx.files.internal("SpriteSheet4.png")));
+		testAnim[4] = (new Texture(Gdx.files.internal("SpriteSheet5.png")));
+		cam = new OrthographicCamera(16*10,9*10);
+		batcher.setProjectionMatrix(cam.combined);
 	}
 	
 	@Override
@@ -29,10 +36,14 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void render(float delta) {
+		Gdx.graphics.getGL20().glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.graphics.getGL20().glClearColor(0.8f, 0.8f, 1, 1);
 		frameIndex += delta;
 		batcher.begin();
 		batcher.draw(testAnim[(int)frameIndex],0,0);
 		batcher.end();
+		if((int)frameIndex >= 4)
+			frameIndex = 0;
 	}
 
 	@Override
--- The Owner/desktop/src/com/jett/game/desktop/DesktopLauncher.java
+++ The Owner/desktop/src/com/jett/game/desktop/DesktopLauncher.java
@@ -7,6 +7,10 @@ import com.jett.game.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.title = "Fur Phil";
+		config.resizable = true;
 		new LwjglApplication(new Main(), config);
 	}
 }
