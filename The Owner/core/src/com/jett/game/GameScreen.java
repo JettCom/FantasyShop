00cb39d07648fcebc601e9f21efe69c08c40fa2d
GoopDev <goopdeveloper@gmail.com>
2016-04-19 23:44:11 -0400

    Have to go to bed, can't code. I added a fireplace to the inn

--- The Owner/core/src/com/jett/game/GameScreen.java
+++ The Owner/core/src/com/jett/game/GameScreen.java
@@ -4,16 +4,36 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Array;
 
 public class GameScreen implements Screen{
 	
 	public OrthographicCamera cam;
 	public Player player;
 	
+	public SpriteBatch mainBatch;
+	
+	public Array<Sprite> fireSprites;
+	
+	public float fireSpriteIndex;
+	
 	public GameScreen(){
+		mainBatch = new SpriteBatch();
 		player = new Player(-50,0);
 		cam = new OrthographicCamera(16*10,9*10);
 		player.batcher.setProjectionMatrix(cam.combined);
+		fireSprites = new Array<Sprite>();
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire1.png"))));
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire2.png"))));
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire3.png"))));
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire4.png"))));
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire5.png"))));
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire6.png"))));
+		fireSprites.add(new Sprite(new Texture(Gdx.files.internal("Fire7.png"))));
+		mainBatch.setProjectionMatrix(cam.combined);
 	}
 
 	@Override
@@ -22,6 +42,12 @@ public class GameScreen implements Screen{
 		Gdx.graphics.getGL20().glClearColor(0.8f, 0.8f, 1, 1);
 		player.render();
 		player.logic(delta);
+		fireSpriteIndex += delta*5;
+		mainBatch.begin();
+		mainBatch.draw(fireSprites.get((int)fireSpriteIndex), 0, 0);
+		mainBatch.end();
+		if(fireSpriteIndex >= 6.5f)
+			fireSpriteIndex = 0;
 	}
 
 	@Override
